96652869995476dca3786a383a976083a7bed26e
David Amorim <davidfilipeamorim@gmail.com>
2021-12-23 20:11:53 +0000

    Add start method on Vulkava

--- lib/Vulkava.ts
+++ lib/Vulkava.ts
@@ -1,12 +1,28 @@
 import { EventEmitter } from 'events';
+import Node from 'Node';
 
 export default class Vulkava extends EventEmitter {
   public readonly clientId: string;
+  public nodes: Node[];
 
   constructor(options: VulkavaOptions) {
     super();
 
+    // TODO: verify input
+
     this.clientId = options.clientId;
-    // TODO:
+
+    this.nodes = [];
+
+    for (const nodeOp of options.nodes) {
+      const node = new Node(this, nodeOp);
+      this.nodes.push(node);
+    }
+  }
+
+  public start() {
+    for (const node of this.nodes) {
+      node.connect();
+    }
   }
 }
